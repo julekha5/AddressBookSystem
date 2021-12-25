85b816c230cfed5b7fd7adaa5ad8d154f8d5ed98
Julekha5 <mulani.julekha93@gmail.com>
2021-12-25 12:39:39 +0530

    Added : Create Contact function code

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -1,10 +1,71 @@
 package com.bridgelabz.addressbook;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBook {
 	
-	public static void main(String[] args){ 
+	List<Contact> listObject = new ArrayList<Contact>();
+	static Scanner scanner = new Scanner(System.in);
+	Contact contact = new Contact(toString(), toString(), toString(), toString(), toString(), toString(), toString(),
+			toString());
+
+	/**
+	 * create new contact method
+	 */
+	public void createContact(Contact con) {
+		System.out.println("Enter Person Details:");
+
+		System.out.println("Enter first Name");
+		String firstName = scanner.next();
+
+		System.out.println("Enter last Name");
+		String lastName = scanner.next();
+
+		System.out.println("Enter Email");
+		String email = scanner.next();
+
+		System.out.println("Enter phone number");
+		String phoneNumber = scanner.next();
+
+		System.out.println("Enter address");
+		String address = scanner.next();
+
+		System.out.println("Enter state");
+		String state = scanner.next();
+
+		System.out.println("Enter city");
+		String city = scanner.next();
+
+		System.out.println("Enter zip");
+		String zip = scanner.next();
+
+		listObject.add(new Contact(firstName, lastName, email, phoneNumber, address, state, city, zip));
+		displayContact();
+	}
+
+	// display method
+	public Contact displayContact() {
+		if (listObject.isEmpty()) {
+			System.out.println("No Records!!!");
+		} else {
+			for (Contact contacts : listObject) {
+				System.out.println(contacts);
+			}
+		}
+		return contact;
+	}
+
+	public static void main(String[] args) {
 		System.out.println("*******************************");
 		System.out.println("Welcome to AddressBook Program");
 		System.out.println("*******************************");
+
+		AddressBook addressbook = new AddressBook();
+		System.out.println("Create New Contact");
+		addressbook.createContact(null);
+
 	}
+
 }
